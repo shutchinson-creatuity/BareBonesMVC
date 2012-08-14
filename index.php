2b62d3411f0e708f468213692ad6c00de25979f6
Scott Hutchinson <liteplate@gmail.com>
2012-08-14 01:04:51 -0500

    Styling updates, Added comments to index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 
+# Error Reporting; Erase in live environment
 ini_set('display_errors', 1);
 error_reporting(-1);
 
+# Define global variables
 define('DOC_ROOT', __DIR__ . '/');
 define('WEB_ROOT', 'http://crunchbang.home');
 
+# Load the library
 require_once DOC_ROOT . '/lib/Bones/Bones.php';
 
 $router = new \Bones\Controller\Router;
