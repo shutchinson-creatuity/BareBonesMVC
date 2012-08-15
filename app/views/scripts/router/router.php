0b0416c7386f3eab5e1dbfa280c131cd2aa14118
Scott Hutchinson <liteplate@gmail.com>
2012-08-15 03:30:50 -0500

    Updated layout and styling of router view

--- app/views/scripts/router/router.php
+++ app/views/scripts/router/router.php
@@ -1,21 +1,15 @@
 <div class="grid_12">
-    <h2>Router Debug</h2><br>
+    <h3>Router Debug</h3><br>
 </div>
-<div class="router_data_header grid_3">
-    <h4>Path:</h4>    
-</div>
+<div class="router_data_header grid_3">Path</div>
 <div class="router_data_container grid_9">
     <?php echo $this->path; ?>
 </div>
-<div class="router_data_header grid_3">
-    <h4>Active Page:</h4>
-</div>
-<div class="router_data_container grid_9">
-    <p><?php echo end($this->urlParams); ?></p>    
-</div>
-<div class="router_data_header grid_3">
-    <h4>URL Variable Count:</h4>
-</div>
+<div class="router_data_header grid_3">URL Parameter Count</div>
 <div class="router_data_container grid_9">
     <p><?php echo count($this->urlParams); ?></p>
+</div>
+<div class="router_data_header grid_3">Active Page</div>
+<div class="router_data_container grid_9">
+    <p><?php echo end($this->urlParams); ?></p>
 </div>
