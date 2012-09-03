1ca0faf4909650a2c9a5eb67f7f560f140803eb1
Scott Hutchinson <liteplate@gmail.com>
2012-09-03 18:47:08 -0500

    Renamed FrontController to SiteController, moved Application startup to SiteController

--- index.php
+++ index.php
@@ -5,42 +5,9 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 # Define global variables
-define('BASE_DIR', __DIR__ . '/');
+defined('BASE_DIR') || define('BASE_DIR', __DIR__ . '/');
 
 # Load the library
 require_once BASE_DIR . '/lib/Bones/Bones.php';
 
-$front = new \Bones\Mvc\Controller\FrontController;
-
-
-
-
-
-
-
-
-
-
-
-
-//$view = new \Bones\Mvc\View\View;
-//$request = new \Bones\Http\Request;
-
-
-
-//$view->getParams = $request->getParam();
-//$view->uriParams = $request->getUriParams();
-//$view->serverParams = $request->serverParam();
-//$view->baseUrl = $request->getBaseUrl();
-//$view->uri = $request->getUri();
-
-//$view->path = $view->render('request/path.php');
-
-//$view->main = $view->render('request/request.php');
-
-//$view->setViewScriptPath('app/layouts/');
-
-////$router = new \Bones\Mvc\Router\Router;
-////$router->route($request);
-
-//echo $view->render('default.php');
+$siteController = new \Bones\Mvc\Controller\SiteController;
--- lib/Bones/Mvc/Controller/SiteController.php
+++ lib/Bones/Mvc/Controller/SiteController.php
@@ -2,20 +2,30 @@
 
 namespace Bones\Mvc\Controller;
 
-class FrontController extends Controller
+class SiteController extends Controller
 {
 
-    private $router;
-    private $request;
-    private $response;
-
+    private $request    = null;
+    private $response   = null;
+    private $router     = null;
+    private $view       = null;
 
 
     public function __construct()
     {
+        $this->view = new \Bones\Mvc\View\View;
+        $this->request = new \Bones\Http\Request;
 
+        $this->view->getParams = $this->request->getParam();
+        $this->view->uriParams = $this->request->getUriParams();
+        $this->view->serverParams = $this->request->serverParam();
+        $this->view->baseUrl = $this->request->getBaseUrl();
+        $this->view->uri = $this->request->getUri();
+        $this->view->path = $this->view->render('request/path.php');
+        $this->view->main = $this->view->render('request/request.php');
 
-
+        $this->view->setViewScriptPath('app/layouts/scripts/');
+        echo $this->view->render('default.php');
     }
 
 
