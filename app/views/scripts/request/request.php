70537b5454f28cbbf159721feafa5586578c5087
Scott Hutchinson <liteplate@gmail.com>
2012-09-02 19:44:50 -0500

    Updated URL rewrite method, Updated 'Request' view script

--- app/views/scripts/request/request.php
+++ app/views/scripts/request/request.php
@@ -14,6 +14,18 @@
     <?php echo $this->path; ?>
 </div>
 <div class="request_data_header grid_3">
+    <h4>URI Parameters:</h4>
+</div>
+<div class="request_data_container grid_9">
+    <?php if (!empty($this->uriParams)): ?>
+        <?php foreach ($this->uriParams as $key => $currentParam): ?>
+            <p><em><?php echo $key; ?></em><span class="dash">&#8211;</span><?php echo $currentParam; ?></p>
+        <?php endforeach; ?>
+    <?php else: ?>
+        &#160;
+    <?php endif; ?>
+</div>
+<div class="request_data_header grid_3">
     <h4>GET Parameters:</h4>
 </div>
 <div class="request_data_container grid_9">
