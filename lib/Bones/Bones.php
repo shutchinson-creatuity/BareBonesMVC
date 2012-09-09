384840e443dba1508b3d60a52808005055ec28dc
Scott Hutchinson <liteplate@gmail.com>
2012-09-08 21:56:19 -0500

    Removed debugging code from 'Bones' class

--- lib/Bones/Bones.php
+++ lib/Bones/Bones.php
@@ -6,22 +6,11 @@ if (!class_exists('\\Bones\Loader\AutoLoader', true)) {
     require_once 'bootstrap.php';
 }
 
-use \Bones\Di\Di;
-use \Bones\Http\Request;
-
-
 class Bones {
 
     private $di = null;
 
     public function __construct()
     {
-        $this->di = new Di;
-        $request = new Request;
-        $this->di->set('request', $request);
-        $uriParams = $this->di->get('request')->uriParam();
-        foreach($uriParams as $currentParam) {
-            echo $currentParam;
-        }
     }
 }
